8fd5440ab889c41261aae0f6724905f9036c804d
platinouss <platinouss@gmail.com>
2022-04-01 22:46:22 +0900

    LinkedList 테스트 코드 추가

--- src/main/java/com/datastructure/MyLinkedList.java
+++ src/main/java/com/datastructure/MyLinkedList.java
@@ -1,4 +1,11 @@
 package com.datastructure;
 
-public class MyLinkedList {
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class MyLinkedList<E> implements List<E> {
+
 }
---
+++ src/main/java/com/datastructure/MyLinkedListTest.java
@@ -0,0 +1,9 @@
+package com.datastructure;
+
+import static org.junit.Assert.*;
+import java.util.ArrayList;
+import org.junit.Before;
+
+public class MyLinkedListTest extends MyArrayListTest{
+
+}
