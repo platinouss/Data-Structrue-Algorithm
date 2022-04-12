97fdd49cf3f2e9d721495b2c4f0fcb3bd045f9ab
platinouss <platinouss@gmail.com>
2022-04-12 18:41:36 +0900

    LinkedList 및 테스트 코드 추가 완료

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Data Structrue & Algorithm
-Using Java (자바로 배우는 핵심 자료구조와 알고리즘)
 
 1. Selection Sort
 2. ArrayList
--- src/main/java/com/datastructure/MyLinkedListTest.java
+++ src/main/java/com/datastructure/MyLinkedListTest.java
@@ -3,7 +3,27 @@ package com.datastructure;
 import static org.junit.Assert.*;
 import java.util.ArrayList;
 import org.junit.Before;
+import java.util.ArrayList;
+import org.junit.Before;
+
+
+/**
+ * @author downey
+ *
+ */
 
 public class MyLinkedListTest extends MyArrayListTest{
+    /**
+     * @throws java.lang.Exception
+     */
+    @Before
+    public void setUp() throws Exception {
+        list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
 
+        mylist = new MyLinkedList<Integer>();
+        mylist.addAll(list);
+    }
 }
